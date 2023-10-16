c42a877b1ca5f6d5d96284176641111d74f2099a
Mehmet Tansu Soydan <aidenshaw.leo@gmail.com>
2023-10-16 00:44:14 -0400

    create search bar, implement search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import "./App.css";
 import { endpoint } from "./api/endpoint";
 import axios from "axios";
 import { Hero } from "./components/hero/hero";
+import { Navbar } from "./components/navbar/navbar";
 
 function App() {
   const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
   console.log(data);
 
   const getInitialData = async () => {
     try {
-      const res = await axios.get(`${endpoint}`);
+      const res = await axios.get(
+        `${endpoint}&q=${searchTerm}&page=${currentPage}`
+      );
       setData(res.data);
     } catch (error) {
       console.log(error);
@@ -23,6 +28,12 @@ function App() {
 
   return (
     <>
+      <Navbar
+        getInitialData={getInitialData}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        setCurrentPage={setCurrentPage}
+      />
       <Hero data={data} />
     </>
   );
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { SearchBar } from "../search-bar/search-bar";
 
-export const navbar = () => {
-  return <div>navbar</div>;
+export const Navbar = ({
+  getInitialData,
+  searchTerm,
+  setSearchTerm,
+  setCurrentPage,
+}) => {
+  return (
+    <div>
+      <SearchBar
+        getInitialData={getInitialData}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
+  );
 };
