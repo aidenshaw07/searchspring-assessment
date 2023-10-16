f637aee3771ac6eff34d6c3ed1c45416ebb6cb91
Mehmet Tansu Soydan <aidenshaw.leo@gmail.com>
2023-10-16 00:14:40 -0400

    render images and item informations

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { CartItem } from "../cart-item/cart-item";
 
-export const hero = () => {
-  return <div>hero</div>;
+export const Hero = ({ data }) => {
+  return (
+    <div>
+      {data.results &&
+        data.results.map((item) => <CartItem key={item.id} item={item} />)}
+    </div>
+  );
 };
