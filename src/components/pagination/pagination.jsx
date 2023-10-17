c7f5952521754371f98fac323198646cee66b805
Mehmet Tansu Soydan <aidenshaw.leo@gmail.com>
2023-10-17 16:50:09 -0400

    add last page and first page buttons

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -13,14 +13,34 @@ export const Pagination = () => {
     setCurrentPage(currentPage - 1);
   };
 
+  const handleFirstPage = () => {
+    setCurrentPage(1);
+  };
+
+  const handleLastPage = () => {
+    setCurrentPage(totalPages);
+  };
+
   return (
     <div>
+      <button
+        hidden={currentPage === 1 || currentPage === 2}
+        onClick={handleFirstPage}
+      >
+        First Page
+      </button>
       <button hidden={currentPage === 1} onClick={handlePreviousPage}>
         Previous Page
       </button>
       {currentPage} of {totalPages}
       <button hidden={currentPage === totalPages} onClick={handleNextPage}>
         Next Page
+      </button>
+      <button
+        hidden={currentPage === totalPages || currentPage === totalPages - 1}
+        onClick={handleLastPage}
+      >
+        Last Page
       </button>
     </div>
   );
