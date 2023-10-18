282569f4477327d7829a6002c61a255ed18e0cbf
Mehmet Tansu Soydan <aidenshaw.leo@gmail.com>
2023-10-17 22:09:32 -0400

    finish navbar styling

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,22 @@
 import { SearchBar } from "../search-bar/search-bar";
 import { useStore } from "../../zustand/store";
+import herboutique from "../../assets/herboutique.svg";
+import shoppingCartIcon from "../../assets/shopping-cart.svg";
+import "./navbar.scss";
 
 export const Navbar = ({ getInitialData }) => {
   const shoppingCart = useStore((state) => state.shoppingCart);
 
   return (
-    <div>
-      <SearchBar getInitialData={getInitialData} />
-      <div>Shopping Cart: {shoppingCart}</div>
+    <div className="navbar-container">
+      <img className="logo" src={herboutique} alt="site-logo" />
+      <div className="search-cart-container">
+        <SearchBar getInitialData={getInitialData} />
+        <div className="shopping-cart-container">
+          <img src={shoppingCartIcon} alt="shopping-cart" />
+          {shoppingCart}
+        </div>
+      </div>
     </div>
   );
 };
